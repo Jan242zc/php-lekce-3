c7cb9984ca6648de17fbf1b066e2dcac9c8f7f3b
Jan242zc <>
2018-10-05 16:02:36 +0000

    tabulka_s_predmety

--- ukol-3.php
+++ ukol-3.php
@@ -13,17 +13,41 @@
 <br>
 <div class="container">
 
-
-
     <table class="table table-bordered">
+    <?php
+    $predmety = ['Ruština' => 1, 'Chemie' => 4, 'Němčina' => 3, 'Biologie' => 5];
+    $tridacervena = ' class="table-danger"';
+        foreach ($predmety as $predmet => $znamka){
+        if ($znamka < 4) {
+        echo"
         <tr>
-            <td>prvni sloupec</td>
-            <td>druhy sloupec</td>
-        </tr>
+            <td>$predmet</td>
+            <td>$znamka</td>
+        </tr>";}
+        else {
+         echo"
+        <tr$tridacervena>
+            <td>$predmet</td>
+            <td>$znamka</td>
+        </tr>"; 
+        }
+    }?>
     </table>
-
-
-
+    <?php
+    /* Výpočet průměru - zadání hovoří o "spočítaném průměru", z čehož si nedokážu vyložit, jestli úkolem je i napsat část
+    kódu, která jej spočítá, aby se ušetřila práce vyvojáře v případě přidání nového předmětu nebo úpravy známky jednoho 
+    ze stávajících předmětů. Každopádně jsem neodolal pokušení zamyslet se nad tím, jak by se toto dalo vyřešit
+    pomocí cyklu :))*/
+    $prumer = 0;
+    $soucet = 0;
+    $pocet = 0;
+    foreach ($predmety as $predmet => $znamka){
+        $soucet = $soucet + $znamka;
+        $pocet = $pocet + 1;
+    }
+    $prumer = $soucet / $pocet;
+    echo ("Průměr: " . $prumer);
+    ?>
 
 </div>
 </body>
